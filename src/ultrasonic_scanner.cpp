59547bb7e433cfc2a044f64a4d291da31748ee63
magicole <colemortensen@gmail.com>
2017-07-13 19:38:18 -0400

    added control pin so that all sensors fire at once

--- src/ultrasonic_scanner.cpp
+++ src/ultrasonic_scanner.cpp
@@ -5,12 +5,15 @@
 #define PORT_ADC1 0
 #define S0 23
 #define S1 24
+#define trigger 22
 
 int init_scanner(){
 	pinMode(S0, OUTPUT);
 	pinMode(S1, OUTPUT);
+	pinMode(trigger, OUTPUT);
 	digitalWrite(S0, LOW);
 	digitalWrite(S1, LOW);
+	digitalWrite(trigger, LOW);
 	return 0;
 }
 
@@ -37,7 +40,7 @@ int scan(int sensorNumber){
 			digitalWrite(S1, LOW);
 			break;
 	}
-	ros::Duration(0.005).sleep();
+	// ros::Duration(0.001).sleep(); // this may not be necessary.
 	return analogRead(PORT_ADC1);
 }
 
@@ -50,12 +53,24 @@ float reading_to_m(int analog_value){
 }
 
 sensor_msgs::LaserScan generate_scan(){
+	//bring trigger high
+	digitalWrite(trigger, HIGH);
+	//wait a bit, we only really need to wait for 20uS but 1ms should be fine
+	ros::Duration(0.001).sleep();
+	//bring the trigger low again so we can trigger it the next time around
+	digitalWrite(trigger, LOW);	
+	//delay for 65ish ms to allow for the sonar to make readings and equalize the analog value
+	ros::Duration(0.065).sleep();	
+	//read the values by scanning through the multiplexer
 	int sensor0_raw = scan(0);
 	int sensor1_raw = scan(1);
 	int sensor2_raw = scan(2);
 	float sensor0_dist = reading_to_m(sensor0_raw);
 	float sensor1_dist = reading_to_m(sensor1_raw);
 	float sensor2_dist = reading_to_m(sensor2_raw);
+
+	
+	//generate the ROS laser scan message
 	ros::Time scan_time = ros::Time::now();
 	sensor_msgs::LaserScan scan;
 	scan.header.stamp = scan_time;
@@ -87,7 +102,7 @@ int main(int argc, char** argv){
 
 	ROS_INFO("Ultrasonic Node Started");
 
-	ros::Rate loop_rate(10);
+	ros::Rate loop_rate(10); //cannot go over 10Hz because of the read rates of our sonars.
 	while(ros::ok()){
 		//doing something
 		/*		
